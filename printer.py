ced5d03ed098ab48938708b48d4aa014e82ffb4e
sergeypuiman <107042276+sergeypuiman@users.noreply.github.com>
2022-06-14 16:13:45 +0400

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,2 +1,6 @@
+import os
+import re
+qualifier = re.sub(r'[-_]+', '', os.environ['BUILD_ID'])[0:12]
+
 print ("---Master branch---")
-print ("---Here's some text from my script---")
+print ("---Build# ", qualifier,"---")
