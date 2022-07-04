07aa55881d296a3b57c5449f1948762d4ea63d95
sergeypuiman <107042276+sergeypuiman@users.noreply.github.com>
2022-07-04 11:45:01 +0400

    Update printer.py

--- printer.py
+++ printer.py
@@ -2,7 +2,7 @@ import os
 import re
 import pyodbc
 
-conn = pyodbc.connect(r'Driver=SQL Server;Server=.\SQLEXPRESS;Database=TRN;Trusted_Connection=yes;')
+conn = pyodbc.connect(r'Driver=FreeTDS;Server=.\SQLEXPRESS;Database=TRN;Trusted_Connection=yes;')
 self.cursor = conn.cursor()
   
 qualifier = re.sub(r'[-_]+', '', os.environ['BUILD_ID'])[0:12]
