51acdad29a990f7480cc048b94f3934418b8c1f6
sergeypuiman <107042276+sergeypuiman@users.noreply.github.com>
2022-07-04 16:02:35 +0400

    Update printer.py

--- printer.py
+++ printer.py
@@ -2,8 +2,6 @@ import os
 import re
 import pyodbc
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ . + ';DATABASE=' + TRN +';UID=' + test + ';PWD=' + test)
-self.cursor = conn.cursor()
   
 qualifier = re.sub(r'[-_]+', '', os.environ['BUILD_ID'])[0:12]
 
