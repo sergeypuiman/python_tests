084de4484d300425648ee731a2206a9c4503b715
sergeypuiman <107042276+sergeypuiman@users.noreply.github.com>
2022-07-04 10:05:34 +0400

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,5 +1,10 @@
 import os
 import re
+import pyodbc
+
+conn = pyodbc.connect(r'Driver=SQL Server;Server=.\SQLEXPRESS;Database=TRN;Trusted_Connection=yes;')
+        self.cursor = conn.cursor()
+  
 qualifier = re.sub(r'[-_]+', '', os.environ['BUILD_ID'])[0:12]
 
 print ("---Master branch---")
